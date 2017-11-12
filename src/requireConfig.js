2658cc2355d2b233b331f5db30fca5d593d516b8
Josh Raker <joshraker@gmail.com>
2017-11-12 16:33:21 -0500

    Updated to use npm instead of bower packages

--- src/requireConfig.js
+++ src/requireConfig.js
@@ -2,20 +2,20 @@ require.config({
     baseUrl: 'js',
     paths: {
         // remove:start
-        jquery: '../vendor/jquery/dist/jquery',
-        knockout: '../vendor/knockout/dist/knockout',
-        'knockout.mapping': '../vendor/knockout-mapping/knockout.mapping',
-        lodash: '../vendor/lodash/lodash',
-        requirejs: '../vendor/requirejs/require',
-        sammy: '../vendor/sammy/lib/sammy'
+        jquery: '../..//node_modules/jquery/dist/jquery',
+        knockout: '../../node_modules/knockout/build/output//knockout-latest.debug',
+        'knockout.mapping': '../../node_modules/knockout-mapping/dist/knockout.mapping',
+        lodash: '../../node_modules/lodash/lodash',
+        requirejs: '../../node_modules/requirejs/require',
+        sammy: '../../node_modules/sammy/lib/sammy'
         // remove:end
         /* remove:line
-        jquery: '../vendor/jquery/dist/jquery.min',
-        knockout: '../vendor/knockout/dist/knockout',
-        'knockout.mapping': '../vendor/knockout-mapping/build/output/knockout.mapping-latest',
-        lodash: '../vendor/lodash/dist/lodash.min',
-        requirejs: '../vendor/requirejs/require',
-        sammy: '../vendor/sammy/lib/min/sammy-latest.min'
+        jquery: '../../node_modules/jquery/dist/jquery.min',
+        knockout: '../../node_modules/knockout/dist/knockout',
+        'knockout.mapping': '../../node_modules/knockout-mapping/build/output/knockout.mapping-latest',
+        lodash: '../../node_modules/lodash/dist/lodash.min',
+        requirejs: '../../node_modules/requirejs/require',
+        sammy: '../../node_modules/sammy/lib/min/sammy-latest.min'
         // remove:line */
     },
     packages: [],
